87012d2779af3ef7913b8e3f84ed2d089692e74d
KubaBoi <jakubanderle@outlook.cz>
2022-08-17 16:32:12 +0200

    debug

--- src/controllers/StreamController.py
+++ src/controllers/StreamController.py
@@ -5,6 +5,7 @@ from Cheese.cheeseController import CheeseController as cc
 from Cheese.cheeseNone import CheeseNone
 from Cheese.Logger import Logger
 
+from src.tools.camera import *
 from src.tools.streamingOutput import StreamingOutput
 
 #@controller /stream;
@@ -30,11 +31,41 @@ class StreamController(cc):
                 server.end_headers()
                 server.wfile.write(frame)
                 server.wfile.write(b"\r\n")
-                server.pause()
+                StreamController.pause()
         except Exception as e:
             Logger.warning(
                 "Removed streaming client %s: %s",
                 server.client_address, str(e))
 
         return CheeseNone()
+
+
+    # METHODS
+
+    def pause():
+        p = "-"
+        if (Camera.counter >= 10):
+            file = open("./status.txt", "r")
+            p = file.read()
+            file.close()
+            Camera.counter = 0
+
+        if (len(p) < 1):
+            p = "-"
+
+        if (p[0] != "-" and p[0] != "1" and p[0] != "0"): #fotecka
+            Camera.stop_recording()
+            Camera.setCamera(True)
+            Camera.capture(p)
+            Camera.start_recording()
+        elif (p[0] == "0"): #zmena framerate
+            Camera.stop_recording()
+            Camera.changeFramerate()
+            Camera.start_recording()
+        elif (p[0] == "1"): #zmena nastaveni
+            Camera.stop_recording()
+            Camera.setCamera(True)
+            Camera.start_recording()
+
+        Camera.counter += 1
         
