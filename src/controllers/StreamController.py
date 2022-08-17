a5c097a649ac185d033d8a348d1f18e008cf1d88
KubaBoi <jakubanderle@outlook.cz>
2022-08-17 17:23:16 +0200

    debug

--- src/controllers/StreamController.py
+++ src/controllers/StreamController.py
@@ -18,20 +18,18 @@ class StreamController(cc):
         server.send_header("Pragma", "no-cache")
         server.send_header("Content-Type", "multipart/x-mixed-replace; boundary=FRAME")
         server.end_headers()
-        try:
-            while True:
-                with StreamingOutput.condition:
-                    StreamingOutput.condition.wait()
-                    frame = StreamingOutput.frame
-                server.wfile.write(b"--FRAME\r\n")
-                server.send_header("Content-Type", "image/jpeg")
-                server.send_header("Content-Length", len(frame))
-                server.end_headers()
-                server.wfile.write(frame)
-                server.wfile.write(b"\r\n")
-                StreamController.pause()
-        except Exception as e:
-            Logger.warning(f"Removed streaming client {server.client_address}: {str(e)}")
+        
+        while True:
+            with StreamingOutput.condition:
+                StreamingOutput.condition.wait()
+                frame = StreamingOutput.frame
+            server.wfile.write(b"--FRAME\r\n")
+            server.send_header("Content-Type", "image/jpeg")
+            server.send_header("Content-Length", len(frame))
+            server.end_headers()
+            server.wfile.write(frame)
+            server.wfile.write(b"\r\n")
+            StreamController.pause()
 
         return CheeseNone()
 
